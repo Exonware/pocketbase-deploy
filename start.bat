15294294de19fc296a61b3a173619f404afe71b6
MUHDASHE <96347530+MUHDASHE@users.noreply.github.com>
2025-04-03 00:26:08 +0300

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,2 @@
+powershell -Command "Expand-Archive -Path 'pocketbase.zip' -DestinationPath '.' -Force"
+start "" "D:\Inetpub\vhosts\exonware-1780.package\pocketbase.exonware.com\pocketbase.exe" serve --http 127.0.0.1:8090
